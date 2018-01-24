eaca5b00709ff5998e380791416cad26951ce23e
zhongjiao <zhongjiao999@163.com>
2018-01-25 00:57:21 +0800

    sample commit

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,9 +1,22 @@
 <template lang="html">
-
+  <el-time-select
+  v-model="value1"
+  :picker-options="{
+    start: '08:30',
+    step: '00:15',
+    end: '18:30'
+  }"
+  placeholder="选择时间">
+</el-time-select>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      value1: ''
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ElementUI from 'element-ui'
 import router from './router'
+import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
