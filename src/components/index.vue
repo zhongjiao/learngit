6195c0d802da3a0a2421cba2d4fd8e47f7c8f5c5
zhongjiao <zhongjiao999@163.com>
2018-01-30 18:29:18 +0800

    office-commit 1/30

--- src/components/index.vue
+++ src/components/index.vue
@@ -7,15 +7,13 @@
       <el-row type="flex" justify="end" class="page-controls">
         <el-col :span="12" class="pagelist-ctl">
           <el-button icon="el-icon-arrow-left">上一页</el-button>
-          <el-button>1</el-button>
-          <el-button>...</el-button>
-          <el-button>2</el-button>
-          <el-button>3</el-button>
-          <el-button>4</el-button>
-          <el-button>5</el-button>
-          <el-button>6</el-button>
-          <el-button>...</el-button>
-          <el-button>7</el-button>
+          <el-button @click="goPage(1)" :class="">1</el-button>
+          <el-button v-if="mdlPageCount[0] > 2">...</el-button>
+          <el-button v-for="count in mdlPageCount"
+                     :key="count"
+                     @click="goPage($event, count)">{{count}}</el-button>
+          <el-button v-if="mdlPageCount[mdlLen-1] < (countTotal-1)">...</el-button>
+          <el-button @click="goPage(pageData.length)">{{pageData.length}}</el-button>
           <el-button>下一页<i class="el-icon-arrow-right el-icon--right"></i></el-button>
         </el-col>
         <el-col :span="12">
@@ -29,11 +27,14 @@
             </el-option>
           </el-select> &nbsp; 条
           到第
-          <input type="number" class="ymy-page-to" v-model="goPage">
+          <input type="number" class="ymy-page-to" v-model="jumpPage">
           页
         </el-col>
       </el-row>
     </el-col>
+    <div style="clear: both">
+      {{this.$data.pageData[this.$data.pageData.length-1]}}
+    </div>
   </el-row>
 </template>
 
@@ -54,13 +55,49 @@ export default {
         value: '500',
         label: '500条'
       }],
-      value: '',
+      pageData: [],
+      value: 10,
       pageNum: 1,
-      goPage: 1,
-      input: 0,
-      pageTotal: 100,
-      countTotal: 1000
+      jumpPage: 1,
+      start: 2,
+      countTotal: 0,
+      mdlLen: 5
     }
+  },
+  created() {
+    for (let i = 0; i < 10; i ++) {
+      this.pageData.push(Math.floor(Math.random() * 101));
+    }
+    this.countTotal = this.pageData.length;
+  },
+  computed: {
+    pageTotal() {
+      return Math.ceil(this.countTotal / this.value);
+    },
+    mdlPageCount() {
+      let tempArr = [];
+      for (let i = 0; i < this.mdlLen; i ++) {
+        tempArr[i] = this.start++;
+      }
+      return tempArr;
+    },
+    isActive() {
+
+    }
+  },
+  watch: {
+
+  },
+  methods: {
+    goPage(event,count) {
+      let avg = Math.floor(this.mdlLen / 2);
+
+      this.mdlPageCount[avg] = count;
+      if((count - avg) > 1 && (count + avg -1) < (this.countTotal-1)) this.start = count - avg;
+    }
+  },
+  mounted: function(){
+
   }
 }
 </script>
@@ -95,7 +132,9 @@ export default {
       height:28px;
       border-radius: 4px;
       border: 1px solid #dcdfe6;
-
+    }
+    .active {
+      color: dodgerblue;
     }
   }
 </style>
